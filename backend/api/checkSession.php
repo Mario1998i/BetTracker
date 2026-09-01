d59bbba592ba1e9aca11daa3d044d95dd4e392b4
Mario1998i <mario.parisella@outlook.it>
2026-09-01 18:39:04 +0200

    Test

--- backend/api/checkSession.php
+++ backend/api/checkSession.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://bettracker0.netlify.app");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
